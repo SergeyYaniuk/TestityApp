b11458951482690ec88fb5ce6901bcfed0882833
SergeyYaniuk <sergeyyaniuk@gmail.com>
2018-09-21 19:18:53 +0300

    Added icon and name of user to toolbar, want to see how it will fit.

--- app/src/main/java/com/sergeyyaniuk/testity/ui/main/MainActivity.java
+++ app/src/main/java/com/sergeyyaniuk/testity/ui/main/MainActivity.java
@@ -2,14 +2,41 @@ package com.sergeyyaniuk.testity.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.sergeyyaniuk.testity.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    TextView mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mAuth = FirebaseAuth.getInstance();
+        mName = (TextView)findViewById(R.id.user_name);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String name = currentUser.getDisplayName();
+        mName.setText(name);
     }
 }
