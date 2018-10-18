e984366151d8f8ea5c15a77bc0f16166c6a89d55
SergeyYaniuk <sergeyyaniuk@gmail.com>
2018-10-18 11:59:08 +0300

    Add code to questionsSwipeCallback

--- app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsAdapter.java
+++ app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsAdapter.java
@@ -1,4 +1,32 @@
 package com.sergeyyaniuk.testity.ui.create;
 
-public class QuestionsAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class QuestionsAdapter extends RecyclerView.Adapter<QuestionsAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsListFragment.java
+++ app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsListFragment.java
@@ -1,4 +1,17 @@
 package com.sergeyyaniuk.testity.ui.create;
 
-public class QuestionsListFragment {
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.widget.FrameLayout;
+
+import java.util.ArrayList;
+
+public class QuestionsListFragment extends Fragment {
+
+    RecyclerView mRecyclerView;
+    QuestionsAdapter mQuestionsAdapter;
+    ArrayList<String> stringArrayList = new ArrayList<>();
+    FrameLayout mFrameLayout;
+
+
 }
