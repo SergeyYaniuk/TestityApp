bd05ed8be9b1ace3929572141564732c9ff8f113
SergeyYaniuk <sergeyyaniuk@gmail.com>
2018-10-18 17:23:56 +0300

    Add code to QuestionsListFragment, Adapter and Holder

--- app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsAdapter.java
+++ app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsAdapter.java
@@ -2,31 +2,72 @@ package com.sergeyyaniuk.testity.ui.create;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sergeyyaniuk.testity.R;
+import com.sergeyyaniuk.testity.data.model.Question;
+
+import java.util.ArrayList;
 
 public class QuestionsAdapter extends RecyclerView.Adapter<QuestionsAdapter.ViewHolder> {
+
+    private ArrayList<Question> mQuestions;
+
+    public QuestionsAdapter(ArrayList<Question> questions){
+        this.mQuestions = questions;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_question, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.mTitle.setText(mQuestions.get(position).getQuestionText());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mQuestions.size();
     }
+
+    //need to be changed
+    public void removeQuestion(int position){
+        mQuestions.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    //need to be changed
+    public void restoreQuestion(Question question, int position){
+        mQuestions.add(position, question);
+        notifyItemInserted(position);
+    }
+
+    //need to be changed
+    public ArrayList<Question> getQuestions(){
+        return mQuestions;
+    }
+
+    //need to be changed
+    public void setQuestions(ArrayList<Question> questions){
+        mQuestions = questions;
+    }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        private TextView mTitle;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            mTitle = itemView.findViewById(R.id.question_title);
         }
     }
 }
--- app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsListFragment.java
+++ app/src/main/java/com/sergeyyaniuk/testity/ui/create/QuestionsListFragment.java
@@ -1,17 +1,114 @@
 package com.sergeyyaniuk.testity.ui.create;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+
+import com.sergeyyaniuk.testity.R;
+import com.sergeyyaniuk.testity.data.model.Question;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/** Need to add comunication with Database */
 public class QuestionsListFragment extends Fragment {
 
+    @BindView(R.id.questionsRecView)
     RecyclerView mRecyclerView;
-    QuestionsAdapter mQuestionsAdapter;
-    ArrayList<String> stringArrayList = new ArrayList<>();
+
+    @BindView(R.id.questions_list_layout)
     FrameLayout mFrameLayout;
 
+    ArrayList<Question> mQuestions;
 
+    QuestionsAdapter mQuestionsAdapter;
+    QuestionsListListener mListener;
+
+    public interface QuestionsListListener{
+        void onQuestionSelected(Uri questionUri);
+        void onAddQuestion();
+    }
+
+    public QuestionsListFragment(){
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_questions_list, container, false);
+        ButterKnife.bind(this, view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mQuestions = new ArrayList<>();
+        updateQuestions();
+        enableSwipe();
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mListener = (QuestionsListListener)context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public void updateQuestions(){
+        if (mQuestionsAdapter == null){
+            mQuestionsAdapter = new QuestionsAdapter(mQuestions);
+            mRecyclerView.setAdapter(mQuestionsAdapter);
+        } else {
+            mQuestionsAdapter.setQuestions(mQuestions);
+            mQuestionsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void enableSwipe(){
+        QuestionsSwipeCallback questionsSwipeCallback = new QuestionsSwipeCallback(getContext()) {  //or getActivity
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                final int position = viewHolder.getAdapterPosition();
+                final Question question = mQuestionsAdapter.getQuestions().get(position);
+                mQuestionsAdapter.removeQuestion(position);
+
+                Snackbar snackbar = Snackbar.make(mFrameLayout, R.string.item_removed, Snackbar.LENGTH_SHORT);
+                snackbar.setAction(R.string.undo, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mQuestionsAdapter.restoreQuestion(question, position);
+                        mRecyclerView.scrollToPosition(position);
+                    }
+                });
+                snackbar.setActionTextColor(Color.YELLOW);
+                snackbar.show();
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(questionsSwipeCallback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
 }
