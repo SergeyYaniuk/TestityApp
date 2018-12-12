84f506fb27d6d6dae35ab4112a8707091759dc45
Sergey Yaniuk <sergeyyaniuk@gmail.com>
2018-12-12 11:22:13 +0200

    Create FirestoreAdapter

--- app/src/main/java/com/sergeyyaniuk/testity/ui/find/findList/adapter/FirestoreAdapter.java
+++ app/src/main/java/com/sergeyyaniuk/testity/ui/find/findList/adapter/FirestoreAdapter.java
@@ -1,4 +1,117 @@
 package com.sergeyyaniuk.testity.ui.find.findList.adapter;
 
-public class FirestoreAdapter {
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
+
+public abstract class FirestoreAdapter<VH extends RecyclerView.ViewHolder>
+        extends RecyclerView.Adapter<VH> implements com.google.firebase.firestore.EventListener<QuerySnapshot> {
+
+    private Query mQuery;
+    private ListenerRegistration mRegistration;
+
+    private ArrayList<DocumentSnapshot> mSnapshots = new ArrayList<>();
+
+    public FirestoreAdapter(Query query) {
+        mQuery = query;
+    }
+
+    public void startListening() {
+        if (mQuery != null && mRegistration == null){
+            mRegistration = mQuery.addSnapshotListener(this);
+        }
+    }
+
+    public void stopListening() {
+        if (mRegistration != null) {
+            mRegistration.remove();
+            mRegistration = null;
+        }
+
+        mSnapshots.clear();
+        notifyDataSetChanged();
+    }
+
+    public void setQuery(Query query) {
+        stopListening();
+        mSnapshots.clear();
+        notifyDataSetChanged();
+
+        // Listen to new query
+        mQuery = query;
+        startListening();
+    }
+
+    @Override
+    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+        //handle errors
+        if (e != null){
+            return;
+        }
+
+        //Dispatch events
+        for (DocumentChange change : queryDocumentSnapshots.getDocumentChanges()){
+            // Snapshot of the changed document
+            DocumentSnapshot snapshot = change.getDocument();
+
+            switch (change.getType()){
+                case ADDED:
+                    onDocumentAdded(change);
+                    break;
+                case MODIFIED:
+                    onDocumentModified(change);
+                    break;
+                case REMOVED:
+                    onDocumentRemoved(change);
+                    break;
+            }
+        }
+        onDataChanged();
+    }
+
+    protected void onDocumentAdded(DocumentChange change){
+        mSnapshots.add(change.getNewIndex(), change.getDocument());
+        notifyItemInserted(change.getNewIndex());
+    }
+
+    protected void onDocumentModified(DocumentChange change){
+        if (change.getOldIndex() == change.getNewIndex()){
+            // Item changed but remained in same position
+            mSnapshots.set(change.getOldIndex(), change.getDocument());
+            notifyItemChanged(change.getOldIndex());
+        }else {
+            // Item changed and changed position
+            mSnapshots.remove(change.getOldIndex());
+            mSnapshots.add(change.getNewIndex(), change.getDocument());
+            notifyItemMoved(change.getOldIndex(), change.getNewIndex());
+        }
+    }
+
+    protected void onDocumentRemoved(DocumentChange change){
+        mSnapshots.remove(change.getOldIndex());
+        notifyItemRemoved(change.getOldIndex());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSnapshots.size();
+    }
+
+    protected DocumentSnapshot getSnapshot(int index) {
+        return mSnapshots.get(index);
+    }
+
+    protected void onError(FirebaseFirestoreException e) {};
+
+    protected void onDataChanged() {}
 }
